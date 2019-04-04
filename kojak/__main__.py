53bcd69185a504d3dac12425af948889980784e5
Natal Ngétal <hobbestigrou@erakis.eu>
2019-04-04 11:03:12 +0200

    List import.
    List all modules import by the module.

--- kojak/__main__.py
+++ kojak/__main__.py
@@ -1,6 +1,22 @@
 import argparse
 import ast
+from collections import namedtuple
 from sys import exit
+
+
+def _get_import(root):
+    Import = namedtuple('Import', ['module', 'name', 'alias'])
+
+    for node in ast.iter_child_nodes(root):
+        if isinstance(node, ast.Import):
+            module = []
+        elif isinstance(node, ast.ImportFrom):
+            module = node.module
+        else:
+            continue
+
+        for name in node.names:
+            yield Import(module, name.name, name.asname)
 
 
 def argparser():
@@ -22,6 +38,7 @@ def main():
     classes = {}
     functions = []
     analyze = ast.parse(filename.read())
+
     for node in analyze.body:
         if 'lineno' not in dir(node) or 'col_offset' not in dir(node):
             continue
@@ -32,8 +49,27 @@ def main():
             for member in node.body:
                 if type(member) is ast.FunctionDef:
                     classes[node.name].append(member.name)
+
+    print('List import by the module:')
+    for imp in _get_import(analyze):
+        result = ''
+
+        if imp.module:
+            result = 'From {module} import {name}'.format(
+                module=imp.module, name=imp.name)
+
+        if not imp.module:
+            result = 'Import {name}'.format(name=imp.name)
+
+        if imp.alias:
+            result += ' with an alias {alias}'.format(alias=imp.alias)
+
+        print('\t-{result}'.format(result=result))
+
     for cls in classes:
+        print('List classes by the module:')
         print(cls)
+
         for meth in classes[cls]:
             print('\t-{}'.format(meth))
 
