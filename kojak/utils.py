64a49fd64ce9d0534cccd2ca15b4a8b6dde15fda
Natal Ngétal <hobbestigrou@erakis.eu>
2019-04-05 14:35:42 +0200

    Display the number import and class by module
    Added a class and convert functions to methods. Displayed the number of
    imports and classes by module.
    
    Fixes: #11

--- kojak/utils.py
+++ kojak/utils.py
@@ -6,30 +6,43 @@ Class = namedtuple('Class', ['node', 'name'])
 Method = namedtuple('Method', ['node', 'name', 'docstring'])
 
 
-def get_imports(root):
-    for node in ast.iter_child_nodes(root):
-        if isinstance(node, ast.Import):
-            module = []
-        elif isinstance(node, ast.ImportFrom):
-            module = node.module
-        else:
-            continue
+class Analyze(object):
+    """To analyze the file."""
 
-        for name in node.names:
-            yield Import(module, name.name, name.asname)
+    count_imports = 0
+    count_classes = 0
 
+    def __init__(self, path):
+        """To initalize the analyze class.
 
-def get_classes(root):
-    for node in ast.iter_child_nodes(root):
-        if isinstance(node, ast.ClassDef):
-            yield Class(node, node.name)
+        @param path: The path of the file or directory to analyze
+        @type path: str
+        """
+        self.path = path
+        self.root = ast.parse(self.path.read())
 
+    def get_imports(self):
+        for node in ast.iter_child_nodes(self.root):
+            if isinstance(node, ast.Import):
+                module = []
+            elif isinstance(node, ast.ImportFrom):
+                module = node.module
+            else:
+                continue
 
-def get_functions(root):
-    for node in ast.iter_child_nodes(root):
-        if isinstance(node, ast.FunctionDef):
-            yield Method(node, node.name, ast.get_docstring(node))
+            for name in node.names:
+                self.count_imports += 1
+                yield Import(module, name.name, name.asname)
 
+    def get_classes(self):
+        for node in ast.iter_child_nodes(self.root):
+            if isinstance(node, ast.ClassDef):
+                self.count_classes += 1
+                yield Class(node, node.name)
 
-def parse_file(file_content):
-    return ast.parse(file_content)
+    def get_functions(self, root=None):
+        root = root if root else self.root
+
+        for node in ast.iter_child_nodes(root):
+            if isinstance(node, ast.FunctionDef):
+                yield Method(node, node.name, ast.get_docstring(node))
